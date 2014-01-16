97b83e9bce477a1f560ffdfeff17f9bc8489e9d4
rasystems <vanroman@web.de>
2014-01-16 16:48:33 +0100

    Update bgfade.js

--- bgfade.js
+++ bgfade.js
@@ -7,34 +7,34 @@
 function startfade()
 {
 	var fadestatus = 0,
-	maxcolors = 4; // Set your colors value
+		maxcolors = 4; // Set your colors value
 
 	setInterval(function(){
 
 		while(fadestatus == 0) // set thes counters
 		{
 			$("body").switchClass( "fadeblue", "fadered", 6000, "easeInOutQuad" );
-			fadestatus =  Math.floor(0 + (1+maxcolors-0)*Math.random());
+			fadestatus =  Math.floor((maxcolors)*Math.random());
 		}
 		while(fadestatus == 1)
 		{
 			$("body").switchClass( "fadered", "fadegreen", 6000, "easeInOutQuad" );
-			fadestatus =  Math.floor(0 + (1+maxcolors-0)*Math.random());
+			fadestatus =  Math.floor((maxcolors)*Math.random());
 		}
 		while(fadestatus == 2)
 		{
 			$("body").switchClass( "fadegreen", "fadeyellow", 6000, "easeInOutQuad" );
-			fadestatus =  Math.floor(0 + (1+maxcolors-0)*Math.random());
+			fadestatus =  Math.floor((maxcolors)*Math.random());
 		}
 		while(fadestatus == 3)
 		{
 			$("body").switchClass( "fadeyellow", "fadecyan", 6000, "easeInOutQuad" );
-			fadestatus =  Math.floor(0 + (1+maxcolors-0)*Math.random());
+			fadestatus =  Math.floor((maxcolors)*Math.random());
 		}
 		while(fadestatus == 4)
 		{
 			$("body").switchClass( "fadecyan", "fadeblue", 6000, "easeInOutQuad" );
-			fadestatus =  Math.floor(0 + (1+maxcolors-0)*Math.random());
+			fadestatus =  Math.floor((maxcolors)*Math.random());
 		}
 		
 	}, 1000);
